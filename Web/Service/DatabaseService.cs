9ad47970c6d029e4735eb944222e79abe77159f4
camilosada12 <camiloandreslosada901@gmail.com>
2025-11-03 20:06:28 -0500

    prueba 21

--- Web/Service/DatabaseService.cs
+++ Web/Service/DatabaseService.cs
@@ -8,24 +8,10 @@ namespace Web.Service
     {
         public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration config)
         {
-            var sql = config.GetConnectionString("SqlServer");
             var pg = config.GetConnectionString("Postgres");
-            var my = config.GetConnectionString("MySql");
 
             // Registrar AuditManager si lo usas en ApplicationDbContext
             // services.AddScoped<AuditService>();
-
-            if (!string.IsNullOrWhiteSpace(sql))
-            {
-                services.AddDbContext<ApplicationDbContext>(opt =>
-                    opt.UseSqlServer(sql, s =>
-                    {
-                        s.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName);
-                        s.EnableRetryOnFailure();
-                        s.CommandTimeout(60);
-                    })
-                );
-            }
 
             if (!string.IsNullOrWhiteSpace(pg))
             {
@@ -36,28 +22,6 @@ namespace Web.Service
                         n.EnableRetryOnFailure();
                         n.CommandTimeout(60);
                     })
-                );
-            }
-
-            if (!string.IsNullOrWhiteSpace(my))
-            {
-                services.AddDbContext<MySqlApplicationDbContext>(opt =>
-                    opt.UseMySql(my, ServerVersion.AutoDetect(my), m =>
-                    {
-                        m.MigrationsAssembly(typeof(MySqlApplicationDbContext).Assembly.FullName);
-
-                        // 👇 Ignorar schemas (MySQL no soporta schemas)
-                        m.SchemaBehavior(MySqlSchemaBehavior.Ignore);
-
-                        // Registrar AuditDbContext si es necesario, con proveedor fijo o dinámico
-                        // services.AddDbContext<AuditDbContext>(options =>
-                        //     options.UseSqlServer(configuration.GetConnectionString("Audit")));
-
-                        // Habilitar traducciones de comparación de strings
-                        m.EnableStringComparisonTranslations();
-                    })
-                    .EnableDetailedErrors()
-                    .EnableSensitiveDataLogging() // ⚠️ solo en desarrollo
                 );
             }
 
